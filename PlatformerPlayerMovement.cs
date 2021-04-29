c22064074ab1ee4a6a26f67569ad68e9911aa46f
Game Builders Club <83189943+GameBuildersClub@users.noreply.github.com>
2021-04-29 12:37:27 -0400

    Created script

---
+++ PlatformerPlayerMovement.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlatformerPlayerMovement : MonoBehaviour
+{
+    // Move player in 2D space
+    public Vector2 speed = new Vector2(3f,6f);
+    public float gravityScale = 1.5f;
+    public float jumpTime = .2f;
+
+    int coinsCollected = 0;
+    bool isGrounded;
+    bool isFalling = true;
+    Vector2 movement,moveDirection;
+    float timeUp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Horizontal Movement
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            moveDirection.x = -1;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            moveDirection.x = 1;
+        }
+        else 
+        {
+            moveDirection.x = 0;
+        }
+        movement.x = moveDirection.x * speed.x;
+
+        if (Input.GetButtonDown("Jump") && isGrounded) 
+        {
+            timeUp = 0;
+            isGrounded = false;
+            isFalling = false;
+        }
+        Jump();
+
+        movement.y = moveDirection.y * speed.y;
+    }
+
+    void Jump() 
+    {
+        if (timeUp == jumpTime) //reached max jump height
+        {
+            isFalling = true;
+        }
+
+        if (!isGrounded)
+        {
+            if (!isFalling)
+            {
+                moveDirection.y = gravityScale * (jumpTime - timeUp);
+                timeUp += Time.deltaTime;
+            }
+            else //isGrounded = false and isFalling = true
+            {
+                moveDirection.y = -gravityScale * (jumpTime - timeUp);
+                timeUp -= Time.deltaTime;
+            }
+        }
+        else //isGrounded = true
+        {
+            moveDirection.y = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        // Coin
+        if (other.gameObject.CompareTag("Coin")) 
+        {
+            coinsCollected += 1;
+            Debug.Log("Coins Collected: " + coinsCollected);
+        }
+
+        if (other.gameObject.CompareTag("Finish")) 
+        {
+            Debug.Log("Changing Scene");
+            SceneManager.LoadScene("GoodEnd");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        // On Ground
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            moveDirection.y = 1;
+        }
+    }
+}
